d066b98e75d01e4b161081218c2fbbd2278c7141
HaneefYaseen <mdhaneefyaseen@gmail.com>
2022-03-21 14:22:13 +0530

    db changes

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const PORT = process.env.PORT || 3000;
-const path = require('path');
-const pool = require('./db')
+const path = require("path");
+const pool = require("./db");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, "build")));
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-})
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
-console.log(path.join(__dirname, 'build'))
+console.log(path.join(__dirname, "build"));
 
-app.post('/users', async function(req, res) {
+app.post("/users", async function (req, res) {
+  try {
     const body = req.body;
     const user = await pool.query(
-        "INSERT INTO users (uid, first_name, last_name, age) VALUES ('" + body.uid + "', '"+body.fname+"', '" + body.lname + "', '" + body.age+"')"
-    )
-    res.json(user)
-})
+      "INSERT INTO users (uid, firstName, lastName, age) VALUES ('" +
+        body.uid +
+        "', '" +
+        body.fname +
+        "', '" +
+        body.lname +
+        "', '" +
+        body.age +
+        "')"
+    );
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-})
+  console.log(`Server listening on port ${PORT}`);
+});
--- src/components/form.js
+++ src/components/form.js
@@ -1,33 +1,56 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function Form() {
+  const [uid, setUid] = useState(0);
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [age, setAge] = useState(0);
 
-    const [uid, setUid] = useState(0)
-    const [fname, setFname] = useState("")
-    const [lname, setLname] = useState("")
-    const [age, setAge] = useState(0)
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const user = {uid, fname, lname, age}
-        const response = await fetch("/users", {
-            method: "POST",
-            headers: { "Content-Type": "application/json"},
-            body: JSON.stringify(user)
-        })
-        console.log(response)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const user = { uid, fname, lname, age };
+      const response = await fetch("/users", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(user),
+      });
+      console.log(response);
+    } catch (err) {
+      console.error(err.message);
     }
-    return (
-        <div className="container">
-            <form method="post" action="" onSubmit={handleSubmit}>
-                <input type="number" placeholder="UID" value={uid} onChange={(e)=>setUid(e.target.value)} />
-                <input type="text" placeholder="FNAME" value={fname} onChange={(e)=>setFname(e.target.value)} />
-                <input type="text" placeholder="LNAME" value={lname} onChange={(e)=>setLname(e.target.value)} />
-                <input type="number" placeholder="AGE" value={age} onChange={(e)=>setAge(e.target.value)} />
-                <button type="submit">Submit</button>
-            </form>
-        </div>
-    )
+  };
+  return (
+    <div className="container">
+      <form method="post" action="" onSubmit={handleSubmit}>
+        <input
+          type="number"
+          placeholder="UID"
+          value={uid}
+          onChange={(e) => setUid(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="FNAME"
+          value={fname}
+          onChange={(e) => setFname(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="LNAME"
+          value={lname}
+          onChange={(e) => setLname(e.target.value)}
+        />
+        <input
+          type="number"
+          placeholder="AGE"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
