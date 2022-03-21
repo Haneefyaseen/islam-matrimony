6366b01822d7b04c430d131f423b79c36cff12d4
HaneefYaseen <mdhaneefyaseen@gmail.com>
2022-03-21 13:59:00 +0530

    db changes

--- db.js
+++ db.js
@@ -2,14 +2,16 @@ const {Pool} = require('pg');
 
 const proConfig = process.env.DATABASE_URL
 
-// const pool = new Pool({
-//     user: 'postgres',
-//     password: 'yaseen',
-//     host: 'localhost',
-//     port: 5427,
-//     database: 'postgres'
-// })
+const devConfig = {
+    user: 'postgres',
+    password: 'yaseen',
+    host: 'localhost',
+    port: 5427,
+    database: 'postgres'
+}
 
-const pool = new Pool(proConfig)
+const pool = new Pool({
+    connectionString: process.env.NODE_ENV === 'production' ? proConfig : devConfig
+})
 
 module.exports = pool;
